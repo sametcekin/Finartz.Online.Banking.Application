ba69a0dcb68183d854279ae26e5f4ac4a1c73315
sametcekin <sametcekin@outlook.com.tr>
2024-05-31 09:55:12 +0300

    #fi: Consumer console application is created

---
+++ src/API.Consumer/SendNotificationWhenAccountCreatedConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Shared;
+
+namespace API.Consumer;
+
+public class SendNotificationWhenAccountCreatedConsumer : IConsumer<AccountCreated>
+{
+    private readonly ILogger<SendNotificationWhenAccountCreatedConsumer> _logger;
+    public async Task Consume(ConsumeContext<AccountCreated> context)
+    {
+        await Task.Delay(1);
+        _logger.LogInformation("Sending notification to user {UserId} that account was created", context.Message.UserId);
+    }
+}
